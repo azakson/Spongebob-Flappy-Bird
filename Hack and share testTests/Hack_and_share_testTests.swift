6619383bd3f065d561f1cf59a5516a6e7ebfbbd8
Avery Zakson <azakson@bsd220.org>
2020-10-20 18:54:38 -0500

    Initial Commit

---
+++ Hack and share testTests/Hack_and_share_testTests.swift
@@ -0,0 +1,34 @@
+//
+//  Hack_and_share_testTests.swift
+//  Hack and share testTests
+//
+//  Created by Avery Zakson on 10/20/20.
+//  Copyright © 2020 Avery Zakson. All rights reserved.
+//
+
+import XCTest
+@testable import Hack_and_share_test
+
+class Hack_and_share_testTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
